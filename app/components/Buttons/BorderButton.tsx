0176639c79b7176e226a990afdd9d8886a8a3d65
Mr.u <usama@coducer.com>
2026-02-24 17:08:44 +0530

    feat: added condition for border button as black

--- app/components/Buttons/BorderButton.tsx
+++ app/components/Buttons/BorderButton.tsx
@@ -8,12 +8,13 @@ interface BorderButton4Props {
   prefixIconChildren?: ReactNode;
   sufixIconChildren?: ReactNode;
   style?: CSSProperties;
+  borderColorWhite?: boolean;
 }
 
-const BorderButton = ({ text, prefixIconChildren, sufixIconChildren, onClick, style }: BorderButton4Props) => {
+const BorderButton = ({ text, prefixIconChildren, sufixIconChildren, onClick, style, borderColorWhite = true }: BorderButton4Props) => {
   return (
     <button
-      className="border-btn border-btn-4 d-flex gap-1 align-items-center"
+      className={` border-btn border-btn-4 d-flex gap-1 align-items-center ${borderColorWhite ? "white-border" : "black-border"}`}
       onClick={onClick}
       style={style}
     >
